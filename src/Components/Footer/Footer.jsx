09c465b9d27912ca50039effc92df3ae37b80546
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-15 21:56:11 +0100

    Completed home page

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>Copryright@2024 Cryptox - All Right Reserved</p>
+        </div>
+    )
+}
+
+export default Footer
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Navbar.css'
+import { CoinContext } from '../../Context/CoinContext';
 
 const Navbar = () => {
+
+    const {setCurrency} = useContext(CoinContext)
+
+    const currencyHandler = (event)=>{
+        switch (event.target.value){
+            case "usd": {
+                setCurrency({name: "usd", symbol: "$"});
+                break;
+            }
+            case "eur": {
+                setCurrency({name: "eur", symbol: "€"});
+                break;
+            }
+            case "inr": {
+                setCurrency({name: "inr", symbol: "₹"});
+                break;
+            }
+            case "ngn": {
+                setCurrency({name: "ngn", symbol: "₦"});
+                break;
+            }
+            default : {
+                setCurrency({name: "usd", symbol: "$"});
+                break;
+            }
+        }
+    }
+
     return (
         <div className='navbar'>
             <img src={'assets/logo.png'} className='logo' />
@@ -12,10 +41,11 @@ const Navbar = () => {
                 <li>Blog</li>
             </ul>
             <div className="nav-right">
-                <select>
+                <select onChange={currencyHandler}>
                     <option value="usd">USD</option>
                     <option value="eur">EUR</option>
                     <option value="inr">INR</option>
+                    <option value="ngn">NGN</option>
                 </select>
                 <button>Sign Up <img src={'assets/arrow_icon.png'} alt="" /></button>
             </div>
